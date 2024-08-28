ae42dd00c18f878347784b9f26231eddcf21e007
Marco Lamina <mlamina09@gmail.com>
2024-08-28 00:09:06 -0700

    Volume control

--- app.py
+++ app.py
@@ -21,7 +21,3 @@ async def read_root(request: Request):
 
     return templates.TemplateResponse("index.html.jinja2", {"request": request})
 
-
-@app.get("/volume-control", response_class=HTMLResponse)
-async def volume_control(request: Request):
-    return templates.TemplateResponse("volume_control.html.jinja2", {"request": request})
